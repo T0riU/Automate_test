e93c7819da30e79263e7f4bc9f126371192606bf
T0riU <112160713+T0riU@users.noreply.github.com>
2023-10-05 15:46:22 +0300

    Added second lab

---
+++ test_lab_2/src/main/java/utils/DriverConfig.java
@@ -0,0 +1,13 @@
+package utils;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources({"classpath:DriverConfig.properties"})
+public interface DriverConfig extends Config{
+    String url();
+    String searchProductUrl();
+    String driver();
+    String email();
+    String password();
+
+}
